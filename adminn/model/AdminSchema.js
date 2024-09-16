537b8c8690ab5be48f9bc1673cb70c4ec787ddae
rajmanavadariya <rajmanavadariya2002@gmail.com>
2024-09-16 15:38:46 +0530

    16-09-2024

---
+++ adminn/model/AdminSchema.js
@@ -0,0 +1,29 @@
+const mongoose=require("mongoose");
+
+
+const userSchema=mongoose.Schema({
+  fname : {
+    type : String,
+    required : true,
+  },
+  email : {
+    type : String,
+    required : true,
+  },
+  password : {
+    type : String,
+    required : true,
+  },
+  msg : {
+    type : String,
+    required : true,
+  },
+  img : {
+    type : String,
+    required : true,
+  },
+});
+
+const UserModel=mongoose.model("data",userSchema);
+
+module.exports=UserModel
