0f848c003ae2d4bbb581e5dcbb02ed28cba7f8aa
rajmanavadariya <rajmanavadariya2002@gmail.com>
2024-10-17 16:04:29 +0530

    17-10-2024

---
+++ api/config/mailer.js
@@ -0,0 +1,20 @@
+const nodemailer = require("nodemailer");
+
+const trasnsport = nodemailer.createTransport({
+    service : "gmail",
+    auth : {
+        user : "rajmanavadariya2002@gmail.com",
+        pass : "sdgjvzccgksmfhte"
+    }
+})
+module.exports.sendotp = (to,otp)=>{
+    let mailoption ={
+        from : "rajmanavadariya2002@gmail.com",
+        to : to,
+        subjet : "Your otp",
+        text : `your otp is ${otp}`
+    }
+    trasnsport.sendMail(mailoption,(err)=>{
+        err && console.log(err);
+    })
+}
